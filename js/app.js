539dc3788b6d303369300d2bbfac251db858924d
Ruud Verbeek <ruud@arfie.nl>
2017-01-19 12:52:57 +0100

    logout button

--- js/app.js
+++ js/app.js
@@ -4,9 +4,33 @@ var crwApp = angular.module('crwApp', ['chart.js', 'ngRoute']);
 crwApp.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
     $locationProvider.hashPrefix('!');
     $routeProvider.when('/home', {
-        templateUrl: 'templates/login.template.html',
-        controller: 'loginController'
+        template: '<login-page onlogin="onlogin()"></login-page>',
     }).when('/rower', {
         template: '<rower-overview></rower-overview>'
     }).otherwise('home');
 }]);
+
+// Main controller
+crwApp.controller('mainController', function($scope, rpc) {
+    $scope.loggedIn = false;
+    $scope.onlogin = function() {
+        $scope.loggedIn = true;
+    };
+
+    if('session' in localStorage)
+        rpc.logged_in().then(function(result) {
+            if(result.result) {
+                $scope.loggedIn = true;
+                window.location = '#!/rower'; // TODO select based on rower/coach
+            } else
+                localStorage.removeItem('session');
+        });
+
+    $scope.logout = function() {
+        rpc.logout().then(function(result) {
+            $scope.loggedIn = false;
+            localStorage.removeItem('session');
+            window.location = '#!/home';
+        });
+    };
+});
